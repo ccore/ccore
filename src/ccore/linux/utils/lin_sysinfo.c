9bdc64910139ccc0f9803ec0ab50a20ad3d0a083
jobtalle <jobtalle@hotmail.com>
2014-10-17 17:07:43 +0200

    Added sysinfo related files

---
+++ src/ccore/linux/utils/lin_sysinfo.c
@@ -0,0 +1,10 @@
+#if defined CC_USE_ALL || defined CC_USE_SYSINFO
+
+#include "lin_sysinfo.h"
+
+ccSysinfo ccSysinfoGet(void)
+{
+
+}
+
+#endif
---
+++ src/ccore/windows/utils/win_sysinfo.c
@@ -0,0 +1,10 @@
+#if defined CC_USE_ALL || defined CC_USE_SYSINFO
+
+#include "win_sysinfo.h"
+
+ccSysinfo ccSysinfoGet(void)
+{
+
+}
+
+#endif
