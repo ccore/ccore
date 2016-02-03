6f60cc8a831cd192e9d23deea7dc6c2006b05adb
jobtalle <jobtalle@hotmail.com>
2016-02-03 13:47:03 +0100

    Updated email

--- include/ccore/types.h
+++ include/ccore/types.h
@@ -6,7 +6,7 @@
 //                             \  \___\___/|_|  \___| 1.1                           //
 //                              \______\                                            //
 //                                                                                  //
-//             Copyright (C) 2014 \ Job Talle (job@ccore.org)                       //
+//             Copyright (C) 2014 \ Job Talle (jobtalle@hotmail.com)                       //
 //                                 \ Thomas Versteeg (thomas@ccore.org)             //
 //__________________________________________________________________________________//
 //                                                                                  //
