c7c808e44eaa52376d74b32300e05af4f9bee4dc
Thomas Versteeg <thomasversteeg@gmx.com>
2016-04-09 12:51:11 +0200

    Add ibus library

--- examples/text.c
+++ examples/text.c
@@ -4,7 +4,16 @@
 
 #include <stdio.h>
 
+#include <ccore/core.h>
+#include <ccore/text.h>
+#include <ccore/window.h>
+#include <ccore/display.h>
+
 int main(int argc, char **argv)
 {
+	ccDisplayInitialize();
+	ccWindowCreate((ccRect){0, 0, 100, 100}, "ccore examples: text", 0);
+
+	ccFree();
 	printf("Hi\n");
 }
